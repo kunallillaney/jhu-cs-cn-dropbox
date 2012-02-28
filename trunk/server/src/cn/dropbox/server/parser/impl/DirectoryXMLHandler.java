eb6640dae8d4fc47f2dbcc4b1e0619c7d9b2dd20
raghuram.sv@gmail.com <raghuram.sv@gmail.com@9d6f9311-a8d3-9205-eb54-973dec826ed5>
2012-02-28 02:41:26 +0000

    Implemented FileXMLHandler and DirectoryXMLHandler

--- trunk/server/src/cn/dropbox/server/parser/impl/DirectoryXMLHandler.java
+++ trunk/server/src/cn/dropbox/server/parser/impl/DirectoryXMLHandler.java
@@ -1,22 +1,201 @@
 package cn.dropbox.server.parser.impl;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
 import cn.dropbox.common.parser.api.XMLHandler;
+import cn.dropbox.common.parser.impl.XMLHelper;
 import cn.dropbox.common.rmgmt.api.Resource;
+import cn.dropbox.common.rmgmt.model.Directory;
+import cn.dropbox.common.rmgmt.model.File;
+import cn.dropbox.common.rmgmt.model.RType;
+import cn.dropbox.common.parser.api.XMLConstants;
+import cn.dropbox.server.parser.XMLUtil;
 
-public class DirectoryXMLHandler implements XMLHandler {
+public class DirectoryXMLHandler implements XMLHandler, XMLConstants {
 
+    /* (non-Javadoc)
+     * @see cn.dropbox.common.parser.api.XMLHandler#constructResourceObject(org.w3c.dom.Document)
+     * Used to construct a resource object from XML when a PUT is called on a Directory. 
+     */
     @Override
     public Resource constructResourceObject(Document xmlDocument) {
-        // TODO Auto-generated method stub
-        return null;
+        Directory dirRes = new Directory();
+        NodeList resElemList = xmlDocument.getElementsByTagName(RESOURCE_TAG);
+        
+        // TODO: Assume that there can only be one resource as of now!
+        Element resElem = (Element)resElemList.item(0);
+        dirRes.setDirName(XMLHelper.getTextValue(resElem, RESOURCE_NAME_TAG));
+        
+        return dirRes;
     }
 
+    /* (non-Javadoc)
+     * @see cn.dropbox.common.parser.api.XMLHandler#constructXML(cn.dropbox.common.rmgmt.api.Resource)
+     * Used to construct a resource object from XML when a GET is called on a Directory. 
+     */
     @Override
-    public String constructXML(Resource Resource) {
-        // TODO Auto-generated method stub
-        return null;
+    public String constructXML(Resource resource) {
+        String retXML = null;
+        Directory dirRes = (Directory)resource;
+        try {
+            Document dom = XMLHelper.getDOMMaker();
+            // create the root element <ResourceList>
+            Element rootEle = dom.createElement(RESOURCE_LIST_TAG);
+            dom.appendChild(rootEle);
+            // 
+            List<File> fileList = dirRes.getFiles();
+            for(File file : fileList) {
+                Element resFileEle = getFileResElement(dom, file);
+                rootEle.appendChild(resFileEle);
+            }
+            
+            List<Directory> dirList = dirRes.getDirectories();
+            for(Directory dir : dirList) {
+                Element resDirEle = getDirResElement(dom, dir);
+                rootEle.appendChild(resDirEle);
+            }
+            retXML=XMLHelper.constructXMLString(dom);
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return retXML;
+    }
+    
+    private Element getDirResElement(Document dom, Directory dir) {
+        Element resourceEle = dom.createElement(RESOURCE_TAG);
+        resourceEle.setAttribute(RESOURCE_ATTR_CATEGORY, RType.DIRECTORY.name().toLowerCase());
+        
+        //ResourceName which is DirectoryName
+        Element resNameEle = dom.createElement(RESOURCE_NAME_TAG);
+        Text resNameText = dom.createTextNode(dir.getDirName());
+        resNameEle.appendChild(resNameText);
+        resourceEle.appendChild(resNameEle);
+        
+        //ResourceSize which is Number of elements
+        Element resSizeEle = dom.createElement(RESOURCE_NUM_ITEMS);
+        Text resSizeText = dom.createTextNode(Integer.toString(dir.getDirSize()));
+        resSizeEle.appendChild(resSizeText);
+        resourceEle.appendChild(resSizeEle);
+        
+        return resourceEle;
+    }
+
+    private Element getFileResElement(Document dom, File file) {
+        Element resourceEle = dom.createElement(RESOURCE_TAG);
+        resourceEle.setAttribute(RESOURCE_ATTR_CATEGORY, RType.FILE.name().toLowerCase());
+        
+        //ResourceName which is FileName
+        Element resNameEle = dom.createElement(RESOURCE_NAME_TAG);
+        Text resNameText = dom.createTextNode(file.getFileName());
+        resNameEle.appendChild(resNameText);
+        resourceEle.appendChild(resNameEle);
+        
+        //ResourceSize which is FileSize
+        Element resSizeEle = dom.createElement(RESOURCE_SIZE_TAG);
+        Text resSizeText = dom.createTextNode(Integer.toString(file.getFileSize()));
+        resSizeEle.appendChild(resSizeText);
+        resourceEle.appendChild(resSizeEle);
+        
+        //ResourceURL which is ResourceURI
+        Element resURLEle = dom.createElement(RESOURCE_SIZE_TAG);
+        Text resURLText = dom.createTextNode(XMLHelper.constructURI(file));
+        resURLEle.appendChild(resURLText);
+        resourceEle.appendChild(resURLEle);
+        
+        //ResourceType which is MimeType
+        Element resTypeEle = dom.createElement(RESOURCE_TYPE_TAG);
+        Text resTypeText = dom.createTextNode(file.getMimeType());
+        resTypeEle.appendChild(resTypeText);
+        resourceEle.appendChild(resTypeEle);
+        
+        //ResourceDate which is Date
+        Element resDateEle = dom.createElement(RESOURCE_DATE_TAG);
+        GregorianCalendar lastModifiedCal = new GregorianCalendar();
+        lastModifiedCal.setTime(file.getLastModified());
+        
+        Element resDateYearEle = dom.createElement(RESOURCE_DATE_YEAR_TAG);
+        Text resDateYearText = dom.createTextNode(Integer.toString(lastModifiedCal.get(Calendar.YEAR)));
+        resDateYearEle.appendChild(resDateYearText);
+        resDateEle.appendChild(resDateYearEle);
+        Element resDateMonthEle = dom.createElement(RESOURCE_DATE_MONTH_TAG);
+        Text resDateMonthText = dom.createTextNode(Integer.toString(lastModifiedCal.get(Calendar.MONTH)));
+        resDateMonthEle.appendChild(resDateMonthText);
+        resDateEle.appendChild(resDateMonthEle);
+        Element resDateDayEle = dom.createElement(RESOURCE_DATE_DAY_TAG);
+        Text resDateDayText = dom.createTextNode(Integer.toString(lastModifiedCal.get(Calendar.DATE)));
+        resDateDayEle.appendChild(resDateDayText);
+        resDateEle.appendChild(resDateDayEle);
+        Element resDateHourEle = dom.createElement(RESOURCE_DATE_HOUR_TAG);
+        Text resDateHourText = dom.createTextNode(Integer.toString(lastModifiedCal.get(Calendar.HOUR)));
+        resDateHourEle.appendChild(resDateHourText);
+        resDateEle.appendChild(resDateHourEle);
+        Element resDateMinuteEle = dom.createElement(RESOURCE_DATE_MINUTE_TAG);
+        Text resDateMinuteText = dom.createTextNode(Integer.toString(lastModifiedCal.get(Calendar.MINUTE)));
+        resDateMinuteEle.appendChild(resDateMinuteText);
+        resDateEle.appendChild(resDateMinuteEle);        Element resDateSecondEle = dom.createElement(RESOURCE_DATE_SECOND_TAG);
+        Text resDateSecondText = dom.createTextNode(Integer.toString(lastModifiedCal.get(Calendar.SECOND)));
+        resDateSecondEle.appendChild(resDateSecondText);
+        resDateEle.appendChild(resDateSecondEle);
+        
+        resourceEle.appendChild(resDateEle);
+        
+        return resourceEle;
+    }
+
+    public static void main(String[] args) {
+        FileInputStream fin;
+        try {
+            fin = new FileInputStream("directoryput.xml");
+            Resource res = XMLUtil.constructResource(fin);
+            System.out.println(res);
+            fin.close();
+            
+            List<File> testFiles = new ArrayList<File>();
+            List<Directory> testDirs = new ArrayList<Directory>();
+            for(int i=0;i<3;i++) {
+                File f = new File();
+                f.setFileName("TestFile"+i);
+                f.setFileSize(i*1024);
+                f.setLastModified(new Date(new Date().getTime() + i*1000*1000*1000));
+                f.setMimeType("Test Mime Type("+i+")");
+                f.setURI("/uri/file"+i);
+                testFiles.add(f);
+                
+                Directory d = new Directory();
+                d.setDirName("DirName"+i);
+                d.setURI("uri/dir"+i);
+                testDirs.add(d);
+            }
+            ((Directory)res).setFiles(testFiles);
+            ((Directory)res).setDirectories(testDirs);
+            System.out.println(XMLUtil.constructXML(res));
+            
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
     }
 
 }
--- trunk/server/src/cn/dropbox/server/parser/impl/FileXMLHandler.java
+++ trunk/server/src/cn/dropbox/server/parser/impl/FileXMLHandler.java
@@ -4,16 +4,20 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.bouncycastle.util.encoders.Base64;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 
+import cn.dropbox.common.parser.api.XMLConstants;
 import cn.dropbox.common.parser.api.XMLHandler;
 import cn.dropbox.common.parser.impl.XMLHelper;
 import cn.dropbox.common.rmgmt.api.Resource;
 import cn.dropbox.common.rmgmt.model.File;
-import cn.dropbox.server.parser.XMLConstants;
+import cn.dropbox.common.rmgmt.model.RType;
 import cn.dropbox.server.parser.XMLUtil;
 
 public class FileXMLHandler implements XMLHandler, XMLConstants {
@@ -29,21 +33,89 @@ public class FileXMLHandler implements XMLHandler, XMLConstants {
         // TODO: Assume that there can only be one resource as of now!
         Element resElem = (Element)resElemList.item(0);
         fileRes.setFileName(XMLHelper.getTextValue(resElem, RESOURCE_NAME_TAG));
-        fileRes.setFileSize(XMLHelper.getTextValue(resElem, RESOURCE_SIZE_TAG));
+        fileRes.setFileSize(XMLHelper.getIntValue(resElem, RESOURCE_SIZE_TAG));
         fileRes.setMimeType(XMLHelper.getTextValue(resElem, RESOURCE_TYPE_TAG));
+        NodeList resDateElemList = resElem.getElementsByTagName(RESOURCE_DATE_TAG);
+        // Assume that there will be only one Date tag
+        Element resDateElem = (Element)resDateElemList.item(0);
+        int year = XMLHelper.getIntValue(resDateElem, RESOURCE_DATE_YEAR_TAG);
+        int month = XMLHelper.getIntValue(resDateElem, RESOURCE_DATE_MONTH_TAG);
+        int day = XMLHelper.getIntValue(resDateElem, RESOURCE_DATE_DAY_TAG);
+        int hour = XMLHelper.getIntValue(resDateElem, RESOURCE_DATE_HOUR_TAG);
+        int min = XMLHelper.getIntValue(resDateElem, RESOURCE_DATE_MINUTE_TAG);
+        int second = XMLHelper.getIntValue(resDateElem, RESOURCE_DATE_SECOND_TAG);
+        fileRes.setLastModified(year, month, day, hour, min, second);
         
-        NodeList resDateElem = resElem.getElementsByTagName(RESOURCE_DATE_TAG);
-        
-        
-        //fileRes.setFileContents(XMLHelper.getTextValue(resElem, RESOURCE_CONTENT_TAG));
+        String fileContentB64 = XMLHelper.getTextValue(resElem, RESOURCE_CONTENT_TAG);
+        fileRes.setFileContents(Base64.decode(fileContentB64));
         
         return fileRes;
     }
 
+    /* (non-Javadoc)
+     * @see cn.dropbox.common.parser.api.XMLHandler#constructXML(cn.dropbox.common.rmgmt.api.Resource)
+     * Used to construct a XML from a resource object when GET is called on a File. 
+     */
     @Override
-    public String constructXML(Resource Resource) {
-        // TODO Auto-generated method stub
-        return null;
+    public String constructXML(Resource resource) {
+        File res = (File)resource;
+        String retXML = null;
+        //get an instance of factory
+        try {
+            Document dom = XMLHelper.getDOMMaker();
+            
+            //create the root element <ResourceDownload>
+            Element rootEle = dom.createElement(RESOURCE_DOWNLOAD_TAG);
+            dom.appendChild(rootEle);
+            
+            Element resourceEle = dom.createElement(RESOURCE_TAG);
+            //resourceEle.setAttribute(RESOURCE_ATTR_CATEGORY, resource.getType().name().toLowerCase());
+            resourceEle.setAttribute(RESOURCE_ATTR_CATEGORY, RType.FILE.name().toLowerCase());
+            
+            //ResourceName which is FileName
+            Element resNameEle = dom.createElement(RESOURCE_NAME_TAG);
+            Text resNameText = dom.createTextNode(res.getFileName());
+            resNameEle.appendChild(resNameText);
+            resourceEle.appendChild(resNameEle);
+            
+            //ResourceSize which is FileSize
+            Element resSizeEle = dom.createElement(RESOURCE_SIZE_TAG);
+            Text resSizeText = dom.createTextNode(Integer.toString(res.getFileSize()));
+            resSizeEle.appendChild(resSizeText);
+            resourceEle.appendChild(resSizeEle);
+            
+            //ResourceType which is MimeType
+            Element resTypeEle = dom.createElement(RESOURCE_TYPE_TAG);
+            Text resTypeText = dom.createTextNode(res.getMimeType());
+            resTypeEle.appendChild(resTypeText);
+            resourceEle.appendChild(resTypeEle);
+            
+            //ResourceEncoding which is always Base64 as of now.
+            Element resEncodingEle = dom.createElement(RESOURCE_ENCODING_TAG);
+            Text resEncodingText = dom.createTextNode(ENCODING_TYPE);
+            resEncodingEle.appendChild(resEncodingText);
+            resourceEle.appendChild(resEncodingEle);
+            
+            //ResourceContent which is FileContents
+            String fileContentsB64 = new String(Base64.encode(res.getFileContents()));
+            Element resContentEle = dom.createElement(RESOURCE_CONTENT_TAG);
+            Text resContentText = dom.createTextNode(fileContentsB64);
+            resContentEle.appendChild(resContentText);
+            resourceEle.appendChild(resContentEle);
+            
+
+            rootEle.appendChild(resourceEle);
+            
+            retXML=XMLHelper.constructXMLString(dom);
+            
+        } catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return retXML;
     }
     
     public static void main(String[] args) {
@@ -53,6 +125,9 @@ public class FileXMLHandler implements XMLHandler, XMLConstants {
             Resource res = XMLUtil.constructResource(fin);
             System.out.println(res);
             fin.close();
+            
+            System.out.println(XMLUtil.constructXML(res));
+            
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
