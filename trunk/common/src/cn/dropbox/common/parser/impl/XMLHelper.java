cbc1bcb4417e713710b6754ffcbd3267dd35ae0a
raghuram.sv@gmail.com <raghuram.sv@gmail.com@9d6f9311-a8d3-9205-eb54-973dec826ed5>
2012-02-28 02:44:02 +0000

    File.java - renamed setDate to setLastModified Directory.java - added toString() Added related XMLHelper, constants and XMLHandler(interface).

--- trunk/common/src/cn/dropbox/common/parser/impl/XMLHelper.java
+++ trunk/common/src/cn/dropbox/common/parser/impl/XMLHelper.java
@@ -1,9 +1,43 @@
 package cn.dropbox.common.parser.impl;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+
+import cn.dropbox.common.rmgmt.model.File;
+
 public class XMLHelper {
+    
+    public static Document getDOMMaker() throws ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        //get an instance of builder
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        //create an instance of DOM
+        Document dom = db.newDocument();
+        return dom;
+    }
+    
+    public static String constructXMLString(Document dom) throws IOException {
+        // Make a string out of the DOM object
+        OutputFormat format = new OutputFormat(dom);
+        format.setIndenting(true);
+        ByteArrayOutputStream byteoutStream = new ByteArrayOutputStream();
+        XMLSerializer serializer = new XMLSerializer(byteoutStream, format);
+        serializer.serialize(dom);
+        String retXMLStr = new String(byteoutStream.toByteArray());
+        byteoutStream.close();
+        return retXMLStr;
+    }
     
     /**
      * I take a xml element and the tag name, look for the tag and get
@@ -37,5 +71,9 @@ public class XMLHelper {
     public static int getIntValue(Element ele, String tagName) {
         //in production application you would catch the exception
         return Integer.parseInt(getTextValue(ele,tagName));
+    }
+    
+    public static String constructURI(File file) {
+        return "DOC_ROOT" + file.getURI(); // TODO: Prepend doc root
     }
 }
