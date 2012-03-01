b2185a57667531f6880546ca65d5637a2b70ae3b
raghuram.sv@gmail.com <raghuram.sv@gmail.com@9d6f9311-a8d3-9205-eb54-973dec826ed5>
2012-03-01 19:02:30 +0000

    Had added ssl properties by mistake.. Removing the same

--- server/src/cn/dropbox/server/listen/ServerListen.java
+++ server/src/cn/dropbox/server/listen/ServerListen.java
@@ -5,10 +5,6 @@ public class ServerListen {
 	public static final String DOCROOT = "C:\\JHU\\Sem2\\03 Computer Networks\\ProgrammingAssignments\\01Dropbox\\testspace";
 	public static void main(String[] args) throws Exception {
 		
-        System.setProperty("javax.net.ssl.keyStore", "C:\\JHU\\Sem2\\keystore.jks");
-        System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
-	    
-	    
 		Thread t = new ReqtListenerThread(8080, DOCROOT);
 		t.setDaemon(false);
 		t.start();
